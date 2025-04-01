78d15bbcc1d3669570003f77d8e6ec8710b16004
matheuscarv69 <matheus9126@gmail.com>
2025-04-01 18:08:59 -0400

    feat: :sparkles: setting postgres and creating docker-compose.yml

---
+++ src/test/java/com/matheuscarv69/spring_security_poc/ASimpleSpringSecurityPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscarv69.spring_security_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ASimpleSpringSecurityPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
