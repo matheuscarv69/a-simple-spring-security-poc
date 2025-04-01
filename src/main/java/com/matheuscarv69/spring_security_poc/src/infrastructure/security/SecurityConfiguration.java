f0752338cf14ad7c8b726324d8f887fd35354a2d
matheuscarv69 <matheus9126@gmail.com>
2025-04-01 19:31:25 -0400

    feat: :sparkles: add SecurityConfiguration for HTTP security and role-based access control

---
+++ src/main/java/com/matheuscarv69/spring_security_poc/src/infrastructure/security/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.matheuscarv69.spring_security_poc.src.infrastructure.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/v1/products")
+                        .hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                        
+                )
+                .build();
+    }
+
+}
