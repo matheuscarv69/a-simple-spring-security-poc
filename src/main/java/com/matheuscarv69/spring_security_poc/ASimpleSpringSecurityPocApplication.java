507b6849ae6ffa6f5fe2237b8a7242e5e72ffa08
matheuscarv69 <matheus9126@gmail.com>
2025-04-01 18:07:56 -0400

    :tada: first commit

---
+++ src/main/java/com/matheuscarv69/spring_security_poc/ASimpleSpringSecurityPocApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscarv69.spring_security_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ASimpleSpringSecurityPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ASimpleSpringSecurityPocApplication.class, args);
+	}
+
+}
