32a140f90c3a7c623bca6fc60ee80f891dbee815
matheuscarv69 <matheus9126@gmail.com>
2025-04-01 20:47:04 -0400

    feat: :sparkles: enhance SecurityConfiguration with AuthenticationManager and PasswordEncoder beans

--- src/main/java/com/matheuscarv69/spring_security_poc/src/infrastructure/security/SecurityConfiguration.java
+++ src/main/java/com/matheuscarv69/spring_security_poc/src/infrastructure/security/SecurityConfiguration.java
@@ -4,9 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Slf4j
@@ -14,6 +18,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    /**
+     * 1. Configures the security filter for the application.
+     * Defines authorization rules and disables CSRF.
+     *
+     * @param httpSecurity HTTP security configuration.
+     * @return The configured SecurityFilterChain.
+     * @throws Exception If an error occurs while configuring the security filter.
+     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
@@ -21,12 +33,44 @@ public class SecurityConfiguration {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/v1/auth/login")
+                        .permitAll()
+                        .requestMatchers(HttpMethod.POST, "/v1/auth/register")
+                        .permitAll()
                         .requestMatchers(HttpMethod.POST, "/v1/products")
                         .hasRole("ADMIN")
                         .anyRequest().authenticated()
-                        
                 )
                 .build();
+    }
+
+    /**
+     * 2. Configures the AuthenticationManager for user authentication.
+     * Used in the AuthenticationController.
+     *
+     * @param authenticationConfiguration Authentication configuration.
+     * @return The configured AuthenticationManager.
+     * @throws Exception If an error occurs while configuring the AuthenticationManager.
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+
+        return authenticationConfiguration.getAuthenticationManager();
+
+    }
+
+    /**
+     * 3. Configures the PasswordEncoder for password hashing.
+     * Used in the UserService.
+     *
+     * @return The configured PasswordEncoder.
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+
     }
 
 }
