94bab443f88fcc7a731d8789ac993f3d876a64e4
matheuscarv69 <matheus9126@gmail.com>
2025-04-01 22:32:21 -0400

    feat: :sparkles: enhance UserService with improved logging and add findUserDetailsByUsername method

--- src/main/java/com/matheuscarv69/spring_security_poc/src/domain/service/UserService.java
+++ src/main/java/com/matheuscarv69/spring_security_poc/src/domain/service/UserService.java
@@ -5,6 +5,7 @@ import com.matheuscarv69.spring_security_poc.src.domain.model.user.UserInputDTO;
 import com.matheuscarv69.spring_security_poc.src.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -19,38 +20,46 @@ public class UserService {
     private final UserRepository userRepository;
 
     public List<User> findAll() {
-        log.info("USER SERVICE - findAll - Iniciando busca de todos os usuários");
+        log.info("USER SERVICE - findAll - Starting to fetch all users");
 
         List<User> users = userRepository.findAll();
 
-        log.info("USER SERVICE - findAll - Finalizando busca de todos os usuários");
+        log.info("USER SERVICE - findAll - Finished fetching all users");
 
         return users;
     }
 
     public Optional<User> findById(Long id) {
-        log.info("USER SERVICE - findById - Iniciando busca de usuário com id: {}", id);
+        log.info("USER SERVICE - findById - Starting to fetch user with id: {}", id);
 
         Optional<User> user = userRepository.findById(id);
 
-        log.info("USER SERVICE - findById - Finalizando busca de usuário com id: {}", id);
+        log.info("USER SERVICE - findById - Finished fetching user with id: {}", id);
 
         return user;
     }
 
-    public User save(UserInputDTO dataInput) {
+    public UserDetails findUserDetailsByUsername(String username) {
 
-        log.info("USER SERVICE - save - Iniciando salvamento de usuário: {}", dataInput);
+        log.info("USER SERVICE - findUserDetailsByUsername - Starting to fetch UserDetails with username: {}", username);
+
+        UserDetails userDetails = userRepository.findByUsername(username);
+
+        log.info("USER SERVICE - findUserDetailsByUsername - Finished fetching UserDetails with username: {}", username);
+
+        return userDetails;
+
+    }
+
+    public User save(UserInputDTO dataInput) {
+        log.info("USER SERVICE - save - Starting to save user: {}", dataInput);
 
         if (userRepository.findByUsername(dataInput.username()) != null) {
-
-            log.error("USER SERVICE - save - Usuário já existe: {}", dataInput.username());
-            throw new RuntimeException("Usuário já existe");
-
+            log.error("USER SERVICE - save - User already exists: {}", dataInput.username());
+            throw new RuntimeException("User already exists");
         }
 
-        User newUser = User
-                .builder()
+        User newUser = User.builder()
                 .username(dataInput.username())
                 .password(new BCryptPasswordEncoder().encode(dataInput.password()))
                 .role(dataInput.role())
@@ -58,19 +67,17 @@ public class UserService {
 
         User savedUser = userRepository.save(newUser);
 
-        log.info("USER SERVICE - save - Usuário salvo: {}", savedUser);
+        log.info("USER SERVICE - save - User saved: {}", savedUser);
 
         return savedUser;
-
     }
 
     public void deleteById(Long id) {
-
-        log.info("USER SERVICE - deleteById - Iniciando exclusão de usuário com id: {}", id);
+        log.info("USER SERVICE - deleteById - Starting to delete user with id: {}", id);
 
         userRepository.deleteById(id);
 
-        log.info("USER SERVICE - deleteById - Usuário com id: {} excluído", id);
-
+        log.info("USER SERVICE - deleteById - User with id: {} deleted", id);
     }
+
 }
