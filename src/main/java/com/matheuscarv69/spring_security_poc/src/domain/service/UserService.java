88bb0ef22ed98eb867e417dbc2bb00bc509b2df4
matheuscarv69 <matheus9126@gmail.com>
2025-04-01 20:46:56 -0400

    feat: :sparkles: update UserService to use UserInputDTO for saving users and add username existence check

--- src/main/java/com/matheuscarv69/spring_security_poc/src/domain/service/UserService.java
+++ src/main/java/com/matheuscarv69/spring_security_poc/src/domain/service/UserService.java
@@ -1,9 +1,11 @@
 package com.matheuscarv69.spring_security_poc.src.domain.service;
 
-import com.matheuscarv69.spring_security_poc.src.domain.model.User;
+import com.matheuscarv69.spring_security_poc.src.domain.model.user.User;
+import com.matheuscarv69.spring_security_poc.src.domain.model.user.UserInputDTO;
 import com.matheuscarv69.spring_security_poc.src.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -36,21 +38,39 @@ public class UserService {
         return user;
     }
 
-    public User save(User user) {
-        log.info("USER SERVICE - save - Iniciando salvamento de usuário: {}", user);
+    public User save(UserInputDTO dataInput) {
 
-        User savedUser = userRepository.save(user);
+        log.info("USER SERVICE - save - Iniciando salvamento de usuário: {}", dataInput);
+
+        if (userRepository.findByUsername(dataInput.username()) != null) {
+
+            log.error("USER SERVICE - save - Usuário já existe: {}", dataInput.username());
+            throw new RuntimeException("Usuário já existe");
+
+        }
+
+        User newUser = User
+                .builder()
+                .username(dataInput.username())
+                .password(new BCryptPasswordEncoder().encode(dataInput.password()))
+                .role(dataInput.role())
+                .build();
+
+        User savedUser = userRepository.save(newUser);
 
         log.info("USER SERVICE - save - Usuário salvo: {}", savedUser);
 
         return savedUser;
+
     }
 
     public void deleteById(Long id) {
+
         log.info("USER SERVICE - deleteById - Iniciando exclusão de usuário com id: {}", id);
 
         userRepository.deleteById(id);
 
         log.info("USER SERVICE - deleteById - Usuário com id: {} excluído", id);
+
     }
 }
